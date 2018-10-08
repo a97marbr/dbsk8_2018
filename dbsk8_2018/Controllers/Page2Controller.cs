59c5a65c09d235bdcd1035cad59e0979deda65ad
Marcus Brohede <a97marbr@gmail.com>
2018-10-08 14:19:10 +0200

    cleanup and seed code 2018

---
+++ dbsk8_2018/Controllers/Page2Controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace dbsk8_2018.Controllers
+{
+    public class Page2Controller : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(new dbsk8_2018.Models.StudyProgramModel().GetAllStudyPrograms());
+        }
+    }
+}
